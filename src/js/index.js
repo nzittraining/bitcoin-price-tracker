0d8f0a3ab5f49e494b6ee2086aee4f49f77267ce
ReganxZheng <frankiexzjwh@gmail.com>
2019-10-03 15:12:02 +1300

    feat: striped and bordered table layout. Fetch info from api

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,41 @@
 import 'bootstrap/dist/css/bootstrap';
 import $ from 'jquery';
+
+const url = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+fetch(url)
+  .then(data => data.json())
+  .then(res => {
+    for (let key in res) {
+      let detail = res[key];
+      switch (key) {
+        case 'chartName':
+          $('#main h1').html(`${detail}`);
+          break;
+        case 'time':
+          $('div.updatedTime').html(`Updated: ${detail.updated}`);
+          break;
+        case 'disclaimer':
+          $('div.description').html(`${detail}`);
+          break;
+        case 'bpi':
+          for (let k in res[key]) {
+            $('#main table')
+              .add(
+                `
+                        <tr>
+                        <td>${detail[k].code}</td>
+                        <td>${detail[k].description}</td>
+                        <td>${detail[k].rate}</td>
+                        <td>${detail[k].symbol}</td>
+                        </tr>
+                    `
+              )
+              .appendTo('#main table');
+          }
+          break;
+      }
+    }
+  })
+  .catch(err => {
+    alert(err);
+  });
